b39f84a3842feba766f217c7c98375ec6a2e91d8
Isaac Helbling <imhelbling@gmail.com>
2018-02-03 19:06:19 -0500

    fix curls

--- install.sh
+++ install.sh
@@ -1,16 +1,17 @@
 git_path=$(which git)
 bin_is_in_path=$(echo "$PATH"|grep -q $HOME/bin && echo true)
 
-install_dir=$HOME/sheepit-cli
-config_path=$HOME/.sheepit_cli_config
+install_dir="${HOME}/sheepit-cli"
+config_path="${HOME}/.sheepit_cli_config"
 
-cli_name=sheepit
-jar_name=sheepit-client-5.590.2883.jar
+cli_name='sheepit'
+jar_name='sheepit-client-5.590.2883.jar'
 
-repo_url=https://github.com/isaachelbling/sheepit-cli.git
-cli_url=https://raw.githubusercontent.com/isaachelbling/sheepit-cli/master/$cli_name
-readme_url=https://raw.githubusercontent.com/isaachelbling/sheepit-cli/master/README.md
-jar_url=https://raw.githubusercontent.com/isaachelbling/sheepit-cli/master/$jar_name
+repo_url='https://github.com/isaachelbling/sheepit-cli.git'
+cli_url="https://raw.githubusercontent.com/isaachelbling/sheepit-cli/master/${cli_name}"
+readme_url='https://raw.githubusercontent.com/isaachelbling/sheepit-cli/master/README.md'
+jar_url="https://raw.githubusercontent.com/isaachelbling/sheepit-cli/master/${jar_name}"
+config_example_url='https://raw.githubusercontent.com/isaachelbling/sheepit-cli/master/config_example'
 
 
 touch $install_dir $config_path
@@ -21,9 +22,10 @@ then
   git clone $repo_url $install_dir
 else
   echo "Git not found, falling back on curl..."
-  curl -O $cli_url $install_dir
-  curl -O $readme_url $install_dir
-  curl -O $jar_url $install_dir
+  curl -o ${cli_url} "${install_dir}/${cli_name}"
+  curl -o ${readme_url} "${install_dir}/README.md"
+  curl -o ${jar_url} "${install_dir}/${jar_name}"
+  curl -o ${config_example_url} "${install_dir}/config_example"
 fi
 
 echo 'Adding SheepIt jar path to CLI config...'

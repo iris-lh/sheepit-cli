a20371e7b6c04068490c87dc98ae3796cd188b75
Isaac Helbling <imhelbling@gmail.com>
2018-02-03 19:31:48 -0500

    make installer more verbose

--- install.sh
+++ install.sh
@@ -11,18 +11,28 @@ repo_url='https://github.com/isaachelbling/sheepit-cli.git'
 cli_url="https://raw.githubusercontent.com/isaachelbling/sheepit-cli/master/${cli_name}"
 readme_url='https://raw.githubusercontent.com/isaachelbling/sheepit-cli/master/README.md'
 jar_url="https://raw.githubusercontent.com/isaachelbling/sheepit-cli/master/${jar_name}"
-config_example_url='https://raw.githubusercontent.com/isaachelbling/sheepit-cli/master/config_example'
+config_example_url="https://raw.githubusercontent.com/isaachelbling/sheepit-cli/master/config_example"
 
 
 if [ ! $git_path = '' ]
 then
   echo "Found Git at ${git_path}."
+  
+  echo "Cloning from: ${repo_url}"
   git clone $repo_url $install_dir
 else
   echo "Git not found, falling back on curl..."
+
+  echo "Source: $cli_url"
   curl -o ${cli_url} "${install_dir}/${cli_name}"
+
+  echo "Source: $readme_url"
   curl -o ${readme_url} "${install_dir}/README.md"
+
+  echo "Source: $jar_url"
   curl -o ${jar_url} "${install_dir}/${jar_name}"
+
+  echo "Source: $config_example_url"
   curl -o ${config_example_url} "${install_dir}/config_example"
 fi
 
@@ -31,6 +41,7 @@ then
   echo "Found config file at ${config_path}"
 else
   echo "Creating config file at ${config_path}/..."
+  echo "Source: $config_example_url"
   curl -o $config_example_url $config_path
 fi
 
